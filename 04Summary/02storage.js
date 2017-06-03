f9e229770227a86ea2d472c5a4b51825cbf516d0
Vanny <diff399202@sina.cn>
2017-06-03 19:29:15 +0800

    完善storage，支持session Storage

--- 04Summary/02storage.js
+++ 04Summary/02storage.js
@@ -2,29 +2,36 @@
  * 存储localStorage
  * @param name 存储名称
  * @param content 存储值
+ * @param method 存储方式：local/session，默认local
  */
-function setStore(name, content) {
+function setStore(name, content, method){
+    method = method || 'localStorage';
     if(!name) return;
     if(typeof content !== 'string'){
         content = JSON.stringify(content);
     }
-    window.localStorage.setItem(name, content);
+    window[method].setItem(name, content);
+    console.log(window[method].getItem(name))
 }
 
 /**
  * 获取localStorage
  * @param name 获取元素的名称
+ * @param method 存储方式：local/session，默认local
  */
-function getStore(name) {
+function getStore(name, method){
+    method = method || 'localStorage';
     if(!name) return;
-    return JSON.parse(window.localStorage.getItem(name));
+    return JSON.parse(window[method].getItem(name));
 }
 
 /**
  * 删除localStorage
  * @param name 删除元素的名称
+ * @param method 存储方式：local/session，默认local
  */
-function removeStore(name) {
+function removeStore(name, method){
+    method = method || 'localStorage';
     if(!name) return;
-    window.localStorage.removeItem(name);
+    window[method].removeItem(name);
 }
